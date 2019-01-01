fcf3400a45a729fd4081b4a3e6c00473b15732f1
caffi-nate <nathan@caffinate.com>
2019-01-02 01:09:55 +0800

    Added javascript hover functionality to grid items to change images

--- scripts.js
+++ scripts.js
@@ -22,4 +22,40 @@ function fixNav() {
   }
 }
 
+/* hover images testing */
+function startPreviewGif(e){
+	let src = e.currentTarget.querySelector('img').src;
+	const newImage = src.replace(".png", ".gif");
+
+	/* only load a gif if we find it in files*/
+	if (doesFileExist(newImage)){
+		e.currentTarget.querySelector('img').src = newImage;
+	}
+}
+
+function stopPreviewGif(e){
+	let src = e.currentTarget.querySelector('img').src;
+	const newImage = src.replace(".gif", ".png");
+	e.currentTarget.querySelector('img').src = newImage;
+}
+
+function doesFileExist(url) {
+	/* TODO: XMLHttpRequest apparently deprecated, find a better method for checking */
+    var xhr = new XMLHttpRequest();
+    xhr.open('HEAD', url, false);
+    xhr.send();
+     
+    if (xhr.status == "404") {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
+
+
 window.addEventListener('scroll', fixNav);
+const items = document.querySelectorAll('.project-item');
+items.forEach(item => item.addEventListener('mouseover', startPreviewGif));
+items.forEach(item => item.addEventListener('mouseout', stopPreviewGif));
