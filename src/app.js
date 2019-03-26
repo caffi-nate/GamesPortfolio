f44ca4612b013f4871b1c5d99e79998db8237b24
caffi-nate <nathan@caffinate.com>
2019-03-26 20:38:18 +0800

    Initial Webpack Setup with Babel and other loaders

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import './scss/style.scss';
+
+// import any images
+
+import brokenImage from "./images/broken.png";
+
+
+import favicon32 from "./images/favicon-32x32.png";
+import faviconIco from "./images/favicon.ico";
+import favicon16 from "./images/favicon-16x16.png";
+
+
+// import any scripts
+import "./javascript/scripts.js";
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,69 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    //entry: './js/app.js',
-    entry: './javascript/scripts.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js'
+    mode: 'development',
+    entry: [
+        './src/app.js'
+    ],
+    watch: true,                                                // watch the files for any changes
+    watchOptions: {
+        aggregateTimeout: 500,                                  // collect all changes and build together
+        poll: 1000,                                             // check file changes ever 1000 milliseconds / 1 second
+        ignored: /node_modules/                                 // ignore folders we don't want to watch
     },
+    devtool: 'source-maps',
+    devServer: {
+        contentBase: path.join(__dirname, 'src'),               // where are the sourcefiles located?
+        watchContentBase: true,
+        hot: true,                                              // hot: hotmr - update page without browser reload
+        open: true,                                             // open page when we start the server
+        inline: true                                            // inject javascript and styles inline
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack Starter Project',
+            template: path.resolve('./src/index.html')
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
     module: {
         rules: [
             {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015']
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    "css-loader",
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(mode_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
                 }
-            }
+            },
+            {
+                test: /\.(jpg|jpeg|gif|png|svg|ico)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        outputPath: './images',
+                        name: "[name].[ext]",
+                    },
+                },
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader:'html-loader',
+                }
+            },
         ]
-    },
-    stats: {
-        colors: true
-    },
-    devtool: 'source-map'
+    }
 };
