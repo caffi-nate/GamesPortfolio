bb2e4f53725c70035a1137c31844ff9b6720ca63
caffi-nate <nathan@caffinate.com>
2019-04-09 22:37:50 +0800

    Slide in text bug fixes

--- src/javascript/scripts.js
+++ src/javascript/scripts.js
@@ -52,28 +52,63 @@ console.log("hello wo2rld");
 
 const sliderImages = document.querySelectorAll('.project-item');
 
-function checkSlide(){
-    //sliderImages.forEach(sliderImage => {
+const sectionTitles = document.querySelectorAll('.section-title');
 
+
+
+
+//https://stackoverflow.com/questions/5598743/finding-elements-position-relative-to-the-document
+// just easier than messing around our old way
+function getCoords(elem) { // crossbrowser version
+    var box = elem.getBoundingClientRect();
+
+    var body = document.body;
+    var docEl = document.documentElement;
+
+    var scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
+    var scrollLeft = window.pageXOffset || docEl.scrollLeft || body.scrollLeft;
+
+    var clientTop = docEl.clientTop || body.clientTop || 0;
+    var clientLeft = docEl.clientLeft || body.clientLeft || 0;
+
+    var top  = box.top +  scrollTop - clientTop;
+    var left = box.left + scrollLeft - clientLeft;
+
+    //return { top: Math.round(top), left: Math.round(left) };
+    return Math.round(top);
+}
+
+function checkSlide(){
+    let slideInAt = (window.pageYOffset + 1*window.innerHeight); // window.pageYOffset is the same as scrollY but works in IE
     for (i = 0; i < sliderImages.length; i++){ // ie fix
         var sliderImage = sliderImages[i];
-    //sliderImages.forEach(function(sliderImage){
-        // halfway through the image
-        //const slideInAt = (window.scrollY + 0.85*window.innerHeight );// - sliderImage.offsetTop;//(window.scrollY + window.innerHeight) - sliderImage.height * 0.5;
-        const slideInAt = (window.pageYOffset + 0.85*window.innerHeight); // window.pageYOffset is the same as scrollY but works in IE
-
-        // bottom of the image
-        const imageBottom = sliderImage.offsetTop + sliderImage.height;
-        const isHalfShown = slideInAt > sliderImage.offsetTop;
-        const isNotScrolledPast = window.scrollY < imageBottom;
-        const isShown = slideInAt > sliderImage.offsetTop;
+        const imageTop = getCoords(sliderImage);
+        let isShown = slideInAt > imageTop;
         if (isShown){ // currently not showing up in Internet Explorer...
             sliderImage.classList.add('item-active');
         }
-    }//);
+    }
+
+
+    for (j = 0; j < sectionTitles.length; j++){
+        const sectionTitle = sectionTitles[j];
+        let slideInAt = (window.pageYOffset + 0.85*window.innerHeight); // window.pageYOffset is the same as scrollY but works in IE
+
+        const titleTop = getCoords(sectionTitle);
+
+        let isShown = slideInAt > titleTop;
+        if (isShown){
+            let titleWords = sectionTitle.querySelectorAll('.title-word');
+            for (k = 0; k < titleWords.length; k++){
+                const titleWord = titleWords[k];
+                titleWord.classList.add("text-in-view");
+            }
+        }
+    }
 }
 
-console.log("hello woasfrld");
+console.log("hello harold");
+
 
 // initialise
 window.onload = function(){
@@ -81,14 +116,6 @@ window.onload = function(){
     checkSlide();
 
 
-    // delay depending on child:
-    const titleWords = document.querySelectorAll('.title-word');
-    console.log(titleWords);
-    //titleWords.forEach(titleWord => {
-    for (i = 0; i < titleWords.length; i++){
-        const titleWord = titleWords[i];
-        titleWord.classList.add("text-in-view");
-    }
 
 }
 
